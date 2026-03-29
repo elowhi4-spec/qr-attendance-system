e7ada6d48de6c5ee70e706f817df06bf1f672385
elowhi4-spec <elowhi4@gmail.com>
2026-03-29 22:48:12 +0800

    Update server.js

--- server.js
+++ server.js
@@ -81,8 +81,10 @@ app.get("/export-word", (req, res) => {
 
 });
 
-app.listen(3000, () => {
-    console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
 
 // RESET ATTENDANCE
